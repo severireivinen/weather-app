14def079332ee1e77d78a08b31d0787db3f9ddeb
severire <severi.reivinen@metropolia.fi>
2022-02-18 15:33:21 +0200

    Cityselector dropdown skeleton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
-import Header from '../components/Header'
+import CitySelector from "../components/CitySelector";
+import Header from "../components/Header";
 
 export default function Home() {
+
   return (
-    <div className='bg-gray-100 min-h-screen flex flex-col'>
+    <div className="bg-gray-100 min-h-screen flex flex-col">
       <Header />
+      <CitySelector />
     </div>
-  )
+  );
 }
