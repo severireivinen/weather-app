d0d2e167ce9454c24265901ce32f2c2e2b7510c7
severire <severi.reivinen@metropolia.fi>
2022-02-18 14:51:15 +0200

    Build init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+   <h1 className='bg-white text-2xl text-center p-3'>Säätutka</h1>
+  )
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className='bg-gray-100 min-h-screen flex flex-col'>
+      <Header />
+    </div>
+  )
+}
